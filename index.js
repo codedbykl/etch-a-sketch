5d1b6c94916515c899ef413f1b5f52c15f5d3d6e
hellokallyne <hellookallyne@gmail.com>
2023-03-10 16:40:12 -0300

    Add event listener to open popup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const sketchPad = document.querySelector('.sketch-pad');
+const buttons = document.querySelectorAll('[data-action]');
 
 function createGrid(sideSquares) {
   const allSquares = sideSquares ** 2;
@@ -8,5 +9,23 @@ function createGrid(sideSquares) {
 
   sketchPad.insertAdjacentHTML('afterbegin', square.repeat(allSquares));
 }
+
+function handleOpenPopup(event) {
+  const popup = event.target.nextElementSibling;
+  popup.classList.remove('hide');
+}
+
+function checkButtonAction(button) {
+  const buttonAction = button.dataset.action;
+
+  if (buttonAction === 'open') {
+    return handleOpenPopup;
+  }
+}
+
+buttons.forEach(button => {
+  const listener = checkButtonAction(button);
+  button.addEventListener('click', event => listener(event));
+});
 
 createGrid(16);
